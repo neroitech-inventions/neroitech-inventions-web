ebdb10e8a63b991ee9f3a55da2cd06cafddcca17
liquidityape <liquidityape@gmail.com>
2025-10-25 15:07:49 +0100

    update
    new commit

--- snappquest-nextjs/src/components/modals/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./BaseModal";
-export * from "./LearnMoreModal";
-export * from "./CreateQuestModal";
-export * from "./CopyConfirmationModal";
-export * from "./JoinQuestModal";
--- snappquest-nextjs/styles.d.ts
+++
@@ -1,2 +0,0 @@
-declare module "@solana/wallet-adapter-react-ui/styles.css";
-declare module "*.css";
